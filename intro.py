5b85fbf2ed2d718655af7ba23609326e520e5e48
kevvinnPY <ketienne0306@gmail.com>
2023-01-20 16:58:38 -0500

    proposed intro to projectRPG
    proposed intro to projectRPG. Small amount of lore and establishes character name and world name

---
+++ intro.py
@@ -0,0 +1,7 @@
+title = 'ProjectRPG'
+name = input("What is your name? ")
+world = input("What is the name of your world? ")
+print("Welcome to",title.upper(),',',name)
+print(name,'...',"long, long ago, there was a majestic hero wielding a mystical weapon who slayed the darkest beast that the world of",world,
+      "has known. You,",name,"defeated the beast JERI F and saved us all. But you did not do it alone, nor did you do it without sacrafice. "
+        "Ah, that look.. You're beginning to remember now, aren't you?")
